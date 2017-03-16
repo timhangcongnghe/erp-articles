06c9824514b972f499a15ddba2791b2216b277a1
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-16 00:36:50 -0700

    update db/seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,39 @@
+# USER
+user = Erp::User.first
+
+# ARTICLES
+puts "Create sample categories article"
+Erp::Articles::Category.where(name: "Chính sách bảo hành").destroy_all
+category1 = Erp::Articles::Category.create(name: "Chính sách bảo hành", alias: Erp::Articles::Category::ALIAS_WARRANTY, creator_id: user.id)
+Erp::Articles::Category.where(name: "Quy định & Hình thức thanh toán").destroy_all
+category2 = Erp::Articles::Category.create(name: "Quy định & Hình thức thanh toán", alias: Erp::Articles::Category::ALIAS_PAYMENT, creator_id: user.id)
+Erp::Articles::Category.where(name: "Chính sách vận chuyển, giao nhận").destroy_all
+category3 = Erp::Articles::Category.create(name: "Chính sách vận chuyển, giao nhận", alias: Erp::Articles::Category::ALIAS_DELIVERY, creator_id: user.id)
+Erp::Articles::Category.where(name: "Chính sách đổi trả hàng và hoàn tiền").destroy_all
+category4 = Erp::Articles::Category.create(name: "Chính sách đổi trả hàng và hoàn tiền", alias: Erp::Articles::Category::ALIAS_RETURN_REFUND, creator_id: user.id)
+Erp::Articles::Category.where(name: "Quy định bảo mật thông tin").destroy_all
+category5 = Erp::Articles::Category.create(name: "Quy định bảo mật thông tin", alias: Erp::Articles::Category::ALIAS_SECURITY, creator_id: user.id)
+Erp::Articles::Category.where(name: "Các điều khoản & Điều kiện").destroy_all
+category6 = Erp::Articles::Category.create(name: "Các điều khoản & Điều kiện", alias: Erp::Articles::Category::ALIAS_TERMS_CONDITIONS, creator_id: user.id)
+Erp::Articles::Category.where(name: "Câu hỏi thường gặp").destroy_all
+category7 = Erp::Articles::Category.create(name: "Câu hỏi thường gặp", alias: Erp::Articles::Category::ALIAS_FAQ, creator_id: user.id)
+
+puts "Create sample articles"
+Erp::Articles::Article.where(name: "Chính sách bảo hành").destroy_all
+article1 = Erp::Articles::Article.create(name: "Chính sách bảo hành", content: "Đang cập nhật...", category_id: category1.id, creator_id: user.id)
+Erp::Articles::Article.where(name: "Quy định & Hình thức thanh toán").destroy_all
+article2 = Erp::Articles::Article.create(name: "Quy định & Hình thức thanh toán", content: "Đang cập nhật...", category_id: category2.id, creator_id: user.id)
+Erp::Articles::Article.where(name: "Chính sách vận chuyển, giao nhận").destroy_all
+article3 = Erp::Articles::Article.create(name: "Chính sách vận chuyển, giao nhận", content: "Đang cập nhật...", category_id: category3.id, creator_id: user.id)
+Erp::Articles::Article.where(name: "Chính sách đổi trả hàng và hoàn tiền").destroy_all
+article4 = Erp::Articles::Article.create(name: "Chính sách đổi trả hàng và hoàn tiền", content: "Đang cập nhật...", category_id: category4.id, creator_id: user.id)
+Erp::Articles::Article.where(name: "Quy định bảo mật thông tin").destroy_all
+article5 = Erp::Articles::Article.create(name: "Quy định bảo mật thông tin", content: "Đang cập nhật...", category_id: category5.id, creator_id: user.id)
+Erp::Articles::Article.where(name: "Điều khoản thứ nhất").destroy_all
+article6 = Erp::Articles::Article.create(name: "Điều khoản thứ nhất", content: "Đang cập nhật...", category_id: category6.id, creator_id: user.id)
+Erp::Articles::Article.where(name: "Điều khoản thứ hai").destroy_all
+article6 = Erp::Articles::Article.create(name: "Điều khoản thứ hai", content: "Đang cập nhật...", category_id: category6.id, creator_id: user.id)
+Erp::Articles::Article.where(name: "Câu hỏi thứ nhất").destroy_all
+article7 = Erp::Articles::Article.create(name: "Câu hỏi thứ nhất", content: "Đang cập nhật...", category_id: category7.id, creator_id: user.id)
+Erp::Articles::Article.where(name: "Câu hoi thứ hai").destroy_all
+article7 = Erp::Articles::Article.create(name: "Câu hỏi thứ hai", content: "Đang cập nhật...", category_id: category7.id, creator_id: user.id)

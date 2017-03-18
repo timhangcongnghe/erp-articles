bb38b3fd4497f284bba3e05d1ad4c0e0630110d5
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-17 21:21:40 -0700

    update comment model

--- app/models/erp/articles/comment.rb
+++ app/models/erp/articles/comment.rb
@@ -1,4 +1,5 @@
 module Erp::Articles
   class Comment < ApplicationRecord
+    validates :name, :email, :message, :presence => true
   end
 end
--- db/migrate/20170310031138_create_erp_articles_comments.rb
+++ db/migrate/20170310031138_create_erp_articles_comments.rb
@@ -1,6 +1,10 @@
 class CreateErpArticlesComments < ActiveRecord::Migration[5.0]
   def change
     create_table :erp_articles_comments do |t|
+      t.string :name
+      t.string :email
+      t.text :message
+      t.references :article, index: true, references: :erp_articles_articles
 
       t.timestamps
     end

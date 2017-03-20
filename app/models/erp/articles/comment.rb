602fa9f8adb21241d34d044f8831e4f9501baead
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-20 00:53:38 -0700

    update comment model: relations tables, filters, search,...

--- app/models/erp/articles/comment.rb
+++ app/models/erp/articles/comment.rb
@@ -1,5 +1,90 @@
 module Erp::Articles
   class Comment < ApplicationRecord
+    belongs_to :article, class_name: "Erp::Articles::Article"
+    belongs_to :parent, class_name: "Erp::Articles::Comment", optional: true
+    has_many :children, class_name: "Erp::Articles::Comment", foreign_key: "parent_id"
     validates :name, :email, :message, :presence => true
+    
+    # Filters
+    def self.filter(query, params)
+      params = params.to_unsafe_hash
+      and_conds = []
+      
+      # show archived items condition - default: false
+			show_archived = false
+			
+			#filters
+			if params["filters"].present?
+				params["filters"].each do |ft|
+					or_conds = []
+					ft[1].each do |cond|
+						# in case filter is show archived
+						if cond[1]["name"] == 'show_archived'
+							# show archived items
+							show_archived = true
+						else
+							or_conds << "#{cond[1]["name"]} = '#{cond[1]["value"]}'"
+						end
+					end
+					and_conds << '('+or_conds.join(' OR ')+')' if !or_conds.empty?
+				end
+			end
+      
+      #keywords
+      if params["keywords"].present?
+        params["keywords"].each do |kw|
+          or_conds = []
+          kw[1].each do |cond|
+            or_conds << "LOWER(#{cond[1]["name"]}) LIKE '%#{cond[1]["value"].downcase.strip}%'"
+          end
+          and_conds << '('+or_conds.join(' OR ')+')'
+        end
+      end
+      
+      # join with articles table for search article
+      query = query.joins(:article)
+      
+      # showing archived items if show_archived is not true
+			query = query.where(archived: false) if show_archived == false
+
+      query = query.where(and_conds.join(' AND ')) if !and_conds.empty?
+      
+      return query
+    end
+    
+    def self.search(params)
+      query = self.all
+      query = self.filter(query, params)
+      
+      # order
+      if params[:sort_by].present?
+        order = params[:sort_by]
+        order += " #{params[:sort_direction]}" if params[:sort_direction].present?
+        
+        query = query.order(order)
+      end
+      
+      return query
+    end
+    
+    def archive
+			update_columns(archived: true)
+		end
+		
+		def unarchive
+			update_columns(archived: false)
+		end
+    
+    def self.archive_all
+			update_all(archived: true)
+		end
+    
+    def self.unarchive_all
+			update_all(archived: false)
+		end
+    
+    def article_name
+			article.present? ? article.name : ''
+		end
   end
 end

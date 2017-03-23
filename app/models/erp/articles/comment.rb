23bb87abae4440d258cd45077fac464d13dc5ab9
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-22 23:57:05 -0700

    update validate format email

--- app/models/erp/articles/comment.rb
+++ app/models/erp/articles/comment.rb
@@ -4,6 +4,7 @@ module Erp::Articles
     belongs_to :parent, class_name: "Erp::Articles::Comment", optional: true
     has_many :children, class_name: "Erp::Articles::Comment", foreign_key: "parent_id"
     validates :name, :email, :message, :presence => true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
     
     # Filters
     def self.filter(query, params)

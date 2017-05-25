85f4dc5b7ed369cad1749f84981756cb6b3575dc
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-05-24 19:58:32 -0700

    backend: update filter/search for articles

--- app/models/erp/articles/article.rb
+++ app/models/erp/articles/article.rb
@@ -5,14 +5,75 @@ module Erp::Articles
     has_many :comments, class_name: "Erp::Articles::Comment", dependent: :destroy
     mount_uploader :image, Erp::Articles::ArticleUploader
     
-    def self.search(params)
-			self.all
-		end
-    
     # get articles active
     def self.get_active
 			self.where(archived: false).order("created_at DESC")
 		end
+    
+    # Filters
+    def self.filter(query, params)
+      params = params.to_unsafe_hash
+      and_conds = []
+
+      # show archived items condition - default: false
+			show_archived = false
+
+			#filters
+			if params["filters"].present?
+				params["filters"].each do |ft|
+					or_conds = []
+					ft[1].each do |cond|
+						# in case filter is show archived
+						if cond[1]["name"] == 'show_archived'
+							# show archived items
+							show_archived = true
+						else
+							or_conds << "#{cond[1]["name"]} = '#{cond[1]["value"]}'"
+						end
+					end
+					and_conds << '('+or_conds.join(' OR ')+')' if !or_conds.empty?
+				end
+			end
+
+      #keywords
+      if params["keywords"].present?
+        params["keywords"].each do |kw|
+          or_conds = []
+          kw[1].each do |cond|
+            or_conds << "LOWER(#{cond[1]["name"]}) LIKE '%#{cond[1]["value"].downcase.strip}%'"
+          end
+          and_conds << '('+or_conds.join(' OR ')+')'
+        end
+      end
+
+      # join with categories table for search with category
+      query = query.joins(:category)
+
+      # join with users table for search creator
+      query = query.joins(:creator)
+
+      # showing archived items if show_archived is not true
+			query = query.where(archived: false) if show_archived == false
+
+      query = query.where(and_conds.join(' AND ')) if !and_conds.empty?
+
+      return query
+    end
+
+    def self.search(params)
+      query = self.all
+      query = self.filter(query, params)
+
+      # order
+      if params[:sort_by].present?
+        order = params[:sort_by]
+        order += " #{params[:sort_direction]}" if params[:sort_direction].present?
+
+        query = query.order(order)
+      end
+
+      return query
+    end
     
     # data for dataselect ajax
     def self.dataselect(keyword='')

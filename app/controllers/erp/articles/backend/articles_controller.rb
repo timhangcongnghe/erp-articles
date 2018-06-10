d791f58ad742f86239eea3e823a4fa6951946324
Ton Hung Nguyen <tonhungstar@gmail.com>
2018-06-10 19:54:31 +0000

    add new permit params

--- app/controllers/erp/articles/backend/articles_controller.rb
+++ app/controllers/erp/articles/backend/articles_controller.rb
@@ -205,7 +205,7 @@ module Erp
           # Only allow a trusted parameter "white list" through.
           def article_params
             params.fetch(:article, {}).permit(:image, :name, :content, :meta_keywords, :meta_description, :tags, :category_id,
-                                              :show_company_structure, :show_brand_identity, :show_our_team)
+                                              :show_company_structure, :show_brand_identity, :show_our_team, :parent_id)
           end
       end
     end
